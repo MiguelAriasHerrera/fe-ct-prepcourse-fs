c93a2f24be6c56459b5b683857a4ff80e3c47542
MiguelAriasHerrera <mxguelzn@gmail.com>
2023-04-06 10:03:24 -0400

    firt commit

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -11,7 +11,7 @@ const nuevoString = "null";
 const nuevoNumero = 7;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = estoyAprendiendo= true;
+const nuevoBoolean = estoyAprendiendo = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - 7 === 3;
